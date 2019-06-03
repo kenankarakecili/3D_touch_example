5cd60337783a6f0a95cb9e89da881bfcbd3b5cd6
kenankarakecili <kenankarakecili@kenankarakecilis-MacBook-Pro.local>
2019-06-03 13:47:55 +1000

    Respond to triggered dynamic actions

--- 3D_touch_example/QuickActionHandler.swift
+++ 3D_touch_example/QuickActionHandler.swift
@@ -11,6 +11,7 @@ import UIKit
 class QuickActionHandler {
 	enum Shortcut: String {
 		case openBlue = "OpenBlue"
+		case favourites = "Favourites"
 	}
 	
 	class func handleQuickAction(shortcutItem: UIApplicationShortcutItem) -> Bool {
@@ -21,6 +22,15 @@ class QuickActionHandler {
 		case .openBlue:
 			getTopmostVC()?.view.backgroundColor = UIColor.blue
 			quickActionHandled = true
+		case .favourites:
+			guard let vc = getTopmostVC() else { return quickActionHandled }
+			var message = "\(shortcutType.rawValue) triggered"
+			if let name = shortcutItem.userInfo?["Name"] {
+				message += " for \(name)"
+			}
+			let alertController = UIAlertController(title: "Quick Action", message: message, preferredStyle: .alert)
+			alertController.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+			vc.present(alertController, animated: true, completion: nil)
 		}
 		return quickActionHandled
 	}
@@ -33,5 +43,16 @@ class QuickActionHandler {
 			return topVC
 		}
 		return nil
+	}
+	
+	class func dynamicActions() -> [UIApplicationShortcutItem] {
+		let actions = [
+			UIApplicationShortcutItem(type: "Favourites",
+									  localizedTitle: "Kenan",
+									  localizedSubtitle: "iOS Developer",
+									  icon: UIApplicationShortcutIcon(type: .contact),
+									  userInfo: ["Name": "Kenan" as NSSecureCoding])
+		]
+		return actions
 	}
 }
